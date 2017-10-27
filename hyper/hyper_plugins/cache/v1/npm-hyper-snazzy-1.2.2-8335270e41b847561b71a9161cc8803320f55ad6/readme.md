be67c02a525934eae192f62249fa44b945b9c387
Guilherme Taschetto <gtaschetto@gmail.com>
2017-10-27 21:59:35 -0200

    Add hyper.js dotfile

---
+++ hyper/hyper_plugins/cache/v1/npm-hyper-snazzy-1.2.2-8335270e41b847561b71a9161cc8803320f55ad6/readme.md
@@ -0,0 +1,30 @@
+# hyper-snazzy
+
+> Elegant [Hyper](https://hyper.is) theme with bright colors
+
+![](screenshot.png)
+
+
+## Install
+
+Add `hyper-snazzy` to the plugins list in your `~/.hyper.js` config file and restart Hyper.
+
+
+## Tip
+
+To get the exact same look as in the screenshot, you need to use the [Pure](https://github.com/sindresorhus/pure) prompt, Menlo font, and the [zsh-syntax-highlighting](https://github.com/zsh-users/zsh-syntax-highlighting) plugin to have commands highlighted.
+
+
+## Related
+
+- [iterm2-snazzy](https://github.com/sindresorhus/iterm2-snazzy) - iTerm2 version
+- [terminal-snazzy](https://github.com/sindresorhus/terminal-snazzy) - Terminal version
+- [konsole-snazzy](https://github.com/miedzinski/konsole-snazzy) - Konsole version
+- [vscode-snazzy](https://github.com/Tyriar/vscode-snazzy) - VS Code version
+- [emacs-snazzy](https://github.com/weijiangan/emacs-snazzy) - Emacs version
+- [termite-snazzy](https://github.com/kbobrowski/termite-snazzy) - Termite version
+
+
+## License
+
+MIT © [Sindre Sorhus](https://sindresorhus.com)
---
+++ hyper/hyper_plugins/node_modules/hyper-snazzy/index.js
@@ -0,0 +1,80 @@
+'use strict';
+const foregroundColor = '#eff0eb';
+const backgroundColor = '#282a36';
+const red = '#ff5c57';
+const green = '#5af78e';
+const yellow = '#f3f99d';
+const blue = '#57c7ff';
+const magenta = '#ff6ac1';
+const cyan = '#9aedfe';
+
+exports.decorateConfig = config => Object.assign({}, config, {
+	backgroundColor,
+	foregroundColor,
+	borderColor: '#222430',
+	cursorColor: '#97979b',
+	colors: {
+		black: backgroundColor,
+		red,
+		green,
+		yellow,
+		blue,
+		magenta,
+		cyan,
+		white: '#f1f1f0',
+		lightBlack: '#686868',
+		lightRed: red,
+		lightGreen: green,
+		lightYellow: yellow,
+		lightBlue: blue,
+		lightMagenta: magenta,
+		lightCyan: cyan,
+		lightWhite: foregroundColor
+	},
+	css: `
+		${config.css}
+
+		/* Hide title when only one tab */
+		.tabs_title {
+			display: none !important;
+		}
+
+		.tab_active:before {
+			border-color: rgba(255, 106, 193, 0.25);
+		}
+
+		.terminal {
+			opacity: 0.6;
+		}
+
+		.terminal.focus {
+			opacity: 1;
+			transition: opacity 0.12s ease-in-out;
+			will-change: opacity;
+		}
+
+		.tab_tab::before {
+			content: '';
+			position: absolute;
+			bottom: 0;
+			left: 0;
+			right: 0;
+			height: 1px;
+			background-color: rgba(255, 106, 193, 0.4);
+			transform: scaleX(0);
+		}
+
+		.tab_first {
+			border-left-color: transparent !important;
+		}
+
+		.tab_tab:not(.tab_active) {
+			color: #666;
+		}
+
+		.tab_tab.tab_active::before {
+			transform: scaleX(1);
+			transition: all 200ms cubic-bezier(0.0, 0.0, 0.2, 1);
+		}
+	`
+});
